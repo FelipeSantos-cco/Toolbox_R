d3f8c35d7cedce02d390f6780245927746933f27
FelipeSantos-cco <felipe.almeida@sptech.school>
2024-01-12 21:48:16 -0300

    Feat: Função de Matriz de Correlação

---
+++ correlacao.R
@@ -0,0 +1,23 @@
+matrix_correlacao <- function(dados.ok){
+  # -> Pacotes Usados
+  library(ggplot2)
+  library(ggcorrplot)
+  
+  cor.data <- round(cor(na.omit(dados.ok)),1)
+  
+  suppressWarnings({ # -> Não exibir mensagens de aviso
+    
+    ggcorrplot(cor.data,
+               legend.title = "Coefficient of \n Correlation",
+               hc.order = T,
+               type = "lower", 
+               lab = T, # ->Exibir Coeficiente de correlação
+               method = "circle",  
+               lab_size = 3, 
+               title = "Correlation between variables", 
+               colors = c("#FF2000", "white", "#01d00d"),  
+               ggtheme=ggplot2::theme_linedraw,
+               digits = 2) 
+  })
+  
+}
